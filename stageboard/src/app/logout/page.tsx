fb072d50df140afaf51f504db93c43df1472a1e4
TimBlokdijk <t.blokdijk@dudegoed.nl>
2025-08-26 14:47:28 +0200

    Logout
    Uit log pagina

---
+++ stageboard/src/app/logout/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { supabaseClient } from '@/lib/supabase/client'
+
+export default function LogoutPage() {
+    const router = useRouter()
+
+    useEffect(function () {
+        async function logoutUser() {
+            await supabaseClient.auth.signOut()
+            router.push('/login')
+        }
+
+        logoutUser()
+    }, [router])
+
+  return <p>Uit loggen...</p>
+}
